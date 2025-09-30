aef6a7b2afacaa9fd10419746f6b3ac1dd95ab1f
Noir <157551923+BinsAksel@users.noreply.github.com>
2025-09-30 01:25:35 -0700

    admin
    admin

--- sumbong/backend/models/User.js
+++ sumbong/backend/models/User.js
@@ -108,11 +108,17 @@ const userSchema = new mongoose.Schema({
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
+  // Only hash if the password field is newly set or modified
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Method to compare password
